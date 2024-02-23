55e9caf564f1017dace95d97d2ea45aaa95d34a5
RAKESH-PUNUGUBATI <rakesh.ponugubati@gmail.com>
2024-02-23 08:44:47 +0530

    added local storage

--- New/src/App.jsx
+++ New/src/App.jsx
@@ -1,8 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
 
 const App = () => {
   const [todos, setTodos] = useState([]);
   const [todoEditing, setTodoEditing] = useState(null);
+  useEffect(()=>{
+    const json  = localStorage.getItem("todos");
+    const loadedTodos = JSON.parse(json);
+    if(loadedTodos){
+      setTodos(loadedTodos);
+    }
+  },[]);
+  useEffect(()=>{
+    if(todos.length > 0){
+      const json = JSON.stringify(todos);
+      localStorage.setItem("todos", json);
+    }
+  }, [todos]);
   
   function handleSubmit(e) {
     e.preventDefault();
